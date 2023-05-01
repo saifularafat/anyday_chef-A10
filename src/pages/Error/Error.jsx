914a6dc25944d4b26b5712e2ca859e1f088bb363
saifularafat <saifularafat.info@gmail.com>
2023-05-02 03:52:13 +0600

    footer and firebase added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            <h2>404</h2>
+        <div className='w-7/12 mx-auto mt-5'>
+            <img src="../../../public/error.png" alt="" />
+            <div className='text-center'>
+                <Link to='/'>
+                    <button
+                        className='border-2 text-2xl
+                                font-medium border-blue-500 
+                                rounded-lg py-2 px-6 my-5 
+                                transition duration-200
+                                hover:bg-blue-100'
+                    >
+                        Go Back
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 };
